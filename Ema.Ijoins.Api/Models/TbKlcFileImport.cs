edf25a946733ee0de9c938cd0b118683d535deca
rattawit pattaracharoenpong <rattawit.dev@gmail.com>
2020-12-15 13:17:15 +0700

    EF PostgreSQL

---
+++ Ema.Ijoins.Api/Models/TbKlcFileImport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Ema.Ijoins.Api.Models
+{
+    public partial class TbKlcFileImport
+    {
+        public int Id { get; set; }
+        public string Filename { get; set; }
+        public DateTime Createdatetime { get; set; }
+    }
+}
---
+++ Ema.Ijoins.Api/Models/ema_databaseContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Ema.Ijoins.Api.Models
+{
+    public partial class ema_databaseContext : DbContext
+    {
+        public ema_databaseContext()
+        {
+        }
+
+        public ema_databaseContext(DbContextOptions<ema_databaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TbKlcFileImport> TbKlcFileImports { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=ema_database;Username=ema_user;Password=magical_password");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "en_US.utf8");
+
+            modelBuilder.Entity<TbKlcFileImport>(entity =>
+            {
+                entity.ToTable("TB_KLC_FILE_IMPORT");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Createdatetime)
+                    .HasColumnName("createdatetime")
+                    .HasDefaultValueSql("now()");
+
+                entity.Property(e => e.Filename)
+                    .IsRequired()
+                    .HasColumnName("filename");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
